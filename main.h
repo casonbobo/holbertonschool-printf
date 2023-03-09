64c75284a4bdad32ecb2f4c51787e68a450cac35
casonbobo <casonsbobo@gmail.com>
2023-03-09 13:30:33 -0800

    poiuy

--- main.h
+++ main.h
@@ -16,7 +16,16 @@ typedef struct printInput
 	char *out;
 } print_t;
 
-
 int _printf(const char *format, ...);
-int _putchar(char c);
+/**accepts a character pointer from printf and calls formatFilter*/
+/*all accepts ANY argument sent to it by format_to_functions*/
+int formatFilter ();
+/*accepts the type of format determined by formatFilter and returns*/
+/*a pointer to the correct function in an array held inside printFunctions*/
+int format_to_function (char *format);
+/**/
+int printFunctions ();
+
+int printChar();
+int printNum();
 #endif
