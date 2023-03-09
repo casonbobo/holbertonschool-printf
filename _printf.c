a20210ffb1e7fc3023c3308a44e8583a5889e8e7
casonbobo <casonsbobo@gmail.com>
2023-03-09 13:25:16 -0800

    alot from my branch

--- _printf.c
+++ _printf.c
@@ -2,19 +2,32 @@
 
 int _printf(const char *format, ...)
 {
-	int f = 0;
-	char *p;
-	va_list clist;
+	const char *p;
+	int i = 0;
+	va_list pointer;
 
-	if (format == '\0')
+	if (!format)
 		return (-1);
+	
+	va_start(pointer, format);
+	for (p = format; *p != NULL, p++)
+	{
+		if (*p != '%')
+		{
+			_putchar(*p, &i)
+			continue;
+		}
+		p++;
+		if (p == NULL)
+                        return (NULL);
+		else if (p == 'c' || 's')
+			return (printChar);
+		else if (p == 'd' || 'i')
+			return (printNum);
+		else if (p == '%')
+			return (printSym);
+		va_end(pointer);
+		return (i);
+	}
 
-	va_start(clist, format)
-
-	while (p[f++])
-		if (p != '%')
-			putchar(p(f));
-	putchar('\n');
-	return (0);
-	va_end(clist);
 }
--- main.h
+++ main.h
@@ -1,15 +1,22 @@
-#define MAIN_H
 #ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stddef.h>
+ /**
+ *struct printInput - is a stucture to
+ *collect the stdout to print
+ *@out: this is the output thats been collected
+ */
 
 typedef struct printInput
 {
-	char *pf;
+	char *out;
 } print_t;
 
 
-int _printf(const char *format, ...)
+int _printf(const char *format, ...);
+int _putchar(char c);
 #endif
