b9dc70615374e970f5276e76dba2e0cf86881b9a
gumquat <gumquat@gmail.com>
2023-03-09 10:26:43 -0800

    :P

--- printer.h
+++ printer.h
@@ -1,5 +1,5 @@
-#define PRINTER_H
-#ifndef PRINTER_H
+#define MAIN_H
+#ifndef MAIN_H
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -11,6 +11,9 @@ typedef struct print
 	void (*f)(char*, ...)
 } print_t;
 
-
-int _printf(const char *format, ...)
+/*int arrPtr - is a pointer to a spot in the array held in format_to_functions*/
+int _printf (const char *format, ...);
+int formatFilter (int *format);
+int format_to_function (int *function);
+int printFunctions (int *arrPtr)
 #endif
