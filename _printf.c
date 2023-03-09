ed42ea4e74580920f0f4c5116187a15441f383cf
casonbobo <casonsbobo@gmail.com>
2023-03-09 10:15:39 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -1,6 +1,20 @@
-#include "printer.h"
+#include "main.h"
 
 int _printf(const char *format, ...)
 {
-	
+	int f = 0;
+	char *p;
+	va_list clist;
+
+	if (format == '\0')
+		return (-1);
+
+	va_start(clist, format)
+
+	while (p[f++])
+		if (p != '%')
+			putchar(p(f));
+	putchar('\n');
+	return (0);
+	va_end(clist);
 }
--- main.h
+++ main.h
@@ -1,14 +1,13 @@
-#define PRINTER_H
-#ifndef PRINTER_H
+#define MAIN_H
+#ifndef MAIN_H
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
-typedef struct print
+typedef struct printInput
 {
-	char *print;
-	void (*f)(char*, ...)
+	char *pf;
 } print_t;
 
 
