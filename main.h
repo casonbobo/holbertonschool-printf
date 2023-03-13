cc8c58bdf25548de73d20257d47bdd59cf952151
casonbobo <casonsbobo@gmail.com>
2023-03-13 09:00:40 -0700

    lets try this. removed main.c and added a proto

--- main.h
+++ main.h
@@ -25,5 +25,6 @@ int print_char(va_list ap);
 int print_str(va_list ap);
 int print_int(va_list ap);
 int print_unsigned_int(unsigned int n);
+int _pow_recursion(int x, int y);
 int _putchar(char c);
 #endif
